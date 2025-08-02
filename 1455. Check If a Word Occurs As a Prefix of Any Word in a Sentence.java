4b496f6fe4d25519cbd47a5423192b6eca8f6fef
123Sarvesh56 <sarveshpatil6477@gmail.com>
2025-08-02 08:07:27 +0530

    leetcode

---
+++ 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] s=sentence.split(" ");
+        for(int i=0;i<s.length;i++){
+            if(s[i].startsWith(searchWord)){
+                return i;
+            }
+        }
+        return -1;
+
+        
+    }
+}
