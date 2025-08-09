8845afa93a3ffbc2f3bb08c01f7bae211f01d7a9
123Sarvesh56 <sarveshpatil6477@gmail.com>
2025-08-09 08:54:02 +0530

    leetcode

---
+++ 1332. Remove Palindromic Subsequences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }else{
+                return 2;
+            }
+        }
+        return 1;
+    }
+}
